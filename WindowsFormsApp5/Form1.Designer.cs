9e7b3277d31f3c1b1e060686edc462a8a7bb8dea
Daniel S <dsilveriodt@gmail.com>
2024-01-22 17:37:24 -0500

    Adding Solution Files

---
+++ WindowsFormsApp5/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace WindowsFormsApp5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.lblOriginalDirectory = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.label4 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.btnOpen.Location = new System.Drawing.Point(415, 141);
+            this.btnOpen.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(133, 28);
+            this.btnOpen.TabIndex = 0;
+            this.btnOpen.Text = "Select";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.btnConvert.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.btnConvert.Location = new System.Drawing.Point(415, 258);
+            this.btnConvert.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(133, 28);
+            this.btnConvert.TabIndex = 1;
+            this.btnConvert.Text = "Convert";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // lblOriginalDirectory
+            // 
+            this.lblOriginalDirectory.Location = new System.Drawing.Point(267, 212);
+            this.lblOriginalDirectory.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.lblOriginalDirectory.Name = "lblOriginalDirectory";
+            this.lblOriginalDirectory.ReadOnly = true;
+            this.lblOriginalDirectory.Size = new System.Drawing.Size(433, 22);
+            this.lblOriginalDirectory.TabIndex = 3;
+            this.lblOriginalDirectory.TextChanged += new System.EventHandler(this.lblOriginalDirectory_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold);
+            this.label1.Location = new System.Drawing.Point(308, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(345, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Welcome to the Blue Ink PDF Converter";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(449, 182);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(86, 16);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "File Location:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.Black;
+            this.label3.Location = new System.Drawing.Point(432, 112);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(103, 16);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Select PDF File:";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.Color.Black;
+            this.label4.Location = new System.Drawing.Point(396, 478);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(178, 16);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "www.github.com/dsilverio123";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::WindowsFormsApp5.Properties.Resources.BlueInk;
+            this.pictureBox1.Location = new System.Drawing.Point(12, -119);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(238, 474);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblOriginalDirectory);
+            this.Controls.Add(this.btnConvert);
+            this.Controls.Add(this.btnOpen);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Blue Ink Converter";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Button btnConvert;
+        private System.Windows.Forms.TextBox lblOriginalDirectory;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ WindowsFormsApp5/Form1.cs
@@ -0,0 +1,208 @@
+﻿using PdfiumViewer;
+using PdfSharp.Pdf;
+using PdfSharp.Pdf.IO;
+using PdfSharp.Drawing;
+using System;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+using PdfDocumentSharp = PdfSharp.Pdf.PdfDocument; // Alias for PdfSharp.Pdf.PdfDocument
+using System.Drawing.Imaging;
+
+namespace WindowsFormsApp5
+{
+    public partial class Form1 : Form
+    {
+        private string selectedPdfPath = string.Empty; // Store the selected PDF file path
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "PDF Files|*.pdf";
+            openFileDialog.Title = "Select a PDF File";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                selectedPdfPath = openFileDialog.FileName;
+                lblOriginalDirectory.Text = "Original PDF Directory: " + Path.GetDirectoryName(selectedPdfPath);
+            }
+        }
+
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(selectedPdfPath))
+            {
+                // Get the directory of the selected PDF file
+                string directory = Path.GetDirectoryName(selectedPdfPath);
+
+                // Generate a new file name in the same directory with "_converted" appended
+                string convertedFileName = Path.GetFileNameWithoutExtension(selectedPdfPath) + "_converted.pdf";
+                string savePdfPath = Path.Combine(directory, convertedFileName);
+
+                // Load the PDF document using PdfiumViewer
+                using (PdfiumViewer.PdfDocument pdfDocument = PdfiumViewer.PdfDocument.Load(selectedPdfPath))
+                {
+                    if (pdfDocument.PageCount > 0)
+                    {
+                        // Create a new PDF document for saving the converted page using PdfSharp
+                        PdfDocumentSharp convertedPdfDocument = new PdfDocumentSharp();
+
+                        for (int pageIndex = 0; pageIndex < pdfDocument.PageCount; pageIndex++)
+                        {
+                            PdfPage convertedPage = convertedPdfDocument.AddPage();
+                            using (XGraphics gfx = XGraphics.FromPdfPage(convertedPage))
+                            {
+                                // Define the output size (you can adjust these values)
+                                int outputWidth = 12000; // Desired width
+                                int outputHeight = 16000; // Desired height
+
+                                // Calculate the scaling factor to fit the PDF content within the output size
+                                float scaleX = (float)outputWidth / pdfDocument.PageSizes[pageIndex].Width;
+                                float scaleY = (float)outputHeight / pdfDocument.PageSizes[pageIndex].Height;
+                                float scale = Math.Min(scaleX, scaleY);
+
+                                // Calculate the new width and height based on the scaling factor
+                                int newWidth = (int)(pdfDocument.PageSizes[pageIndex].Width * scale);
+                                int newHeight = (int)(pdfDocument.PageSizes[pageIndex].Height * scale);
+
+                                // Render the PDF page with the calculated dimensions
+                                Bitmap pageImage = (Bitmap)pdfDocument.Render(pageIndex, newWidth, newHeight, true);
+
+                                if (pageImage != null)
+                                {
+                                    try
+                                    {
+                                        ModifyImage(pageImage); // Apply your logic to modify the image
+
+                                        using (MemoryStream stream = new MemoryStream())
+                                        {
+                                            // Save as TIFF format with compression
+                                            SaveTiffWithCompression(pageImage, stream, EncoderValue.CompressionLZW); // You can adjust the compression level here
+
+                                            // Convert the Bitmap to a stream
+                                            XImage modifiedImage = XImage.FromStream(stream); // Create an XImage from the stream
+
+                                            // Draw the modified image on the converted page
+                                            gfx.DrawImage(modifiedImage, 0, 0, convertedPage.Width, convertedPage.Height);
+                                        }
+                                    }
+                                    finally
+                                    {
+                                        pageImage.Dispose(); // Dispose of the Bitmap to release resources
+                                    }
+                                }
+                            }
+                        }
+
+                        // Save the converted PDF document
+                        convertedPdfDocument.Save(savePdfPath);
+
+                        MessageBox.Show("PDF Converted with Modifications and Saved as PDF!", "Conversion Complete", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    else
+                    {
+                        MessageBox.Show("The PDF has no pages.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please select a PDF file first.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+
+        private void SaveTiffWithCompression(Bitmap bmp, Stream stream, EncoderValue compressionLevel)
+        {
+            ImageCodecInfo tiffCodecInfo = GetTiffEncoderInfo();
+
+            EncoderParameters encoderParams = new EncoderParameters(1);
+            encoderParams.Param[0] = new EncoderParameter(Encoder.Compression, (long)compressionLevel); // Adjust the compression level here
+
+            bmp.Save(stream, tiffCodecInfo, encoderParams);
+        }
+
+        private ImageCodecInfo GetTiffEncoderInfo()
+        {
+            ImageCodecInfo[] codecs = ImageCodecInfo.GetImageEncoders();
+            foreach (ImageCodecInfo codec in codecs)
+            {
+                if (codec.FormatID == ImageFormat.Tiff.Guid)
+                {
+                    return codec;
+                }
+            }
+            return null;
+        }
+
+        private void ModifyImage(Bitmap bmp)
+        {
+            for (int x = 0; x < bmp.Width; x++)
+            {
+                for (int y = 0; y < bmp.Height; y++)
+                {
+                    Color pixelColor = bmp.GetPixel(x, y);
+                    if (!IsWhite(pixelColor))
+                    {
+                        bmp.SetPixel(x, y, Color.Blue);
+                    }
+                }
+            }
+        }
+
+        private bool IsWhite(Color color)
+        {
+            return color.R > 240 && color.G > 240 && color.B > 240;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblOriginalDirectory_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ModifyImage32bppArgb(Bitmap bmp)
+        {
+            for (int x = 0; x < bmp.Width; x++)
+            {
+                for (int y = 0; y < bmp.Height; y++)
+                {
+                    Color pixelColor = bmp.GetPixel(x, y);
+                    if (!IsWhite(pixelColor))
+                    {
+                        bmp.SetPixel(x, y, Color.Blue);
+                    } 
+                }
+            }
+        }
+    }
+}
